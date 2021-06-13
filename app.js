a55a5e64b40038e8383bed26de7074edfad3dae9
Satchit Hari <satchithari1910@gmail.com>
2021-06-13 18:03:33 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -8,23 +9,15 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-var items = ["Finish MoM", "Make chicken Drumsticks"];
+const items = ["Finish MoM", "Make chicken Drumsticks"];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  const item = req.body.newItem;
 
   items.push(item);
 
